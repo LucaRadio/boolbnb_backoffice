49c61544570644e2ecfc120f1f292e043b04ac96
luciobo <luciobo@icloud.com>
2023-03-08 11:32:20 +0100

    added old on form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,32 +12,43 @@
                                 <label for="name"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        value="{{ old('name') }}">
                                 </div>
                                 @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
                             <div class="mb-4 row">
                                 <label for="surname"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
                                 <div class="col-md-6">
-                                    <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" value="{{ old('surname') }}">
+                                    <input id="surname" type="text"
+                                        class="form-control @error('surname') is-invalid @enderror"
+                                        value="{{ old('surname') }}">
                                 </div>
                                 @error('surname')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
                                 @enderror
                             </div>
                             <div class="mb-4 row">
                                 <label for="date_of_birth"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Date of birth') }}</label>
                                 <div class="col-md-6">
-                                    <input id="date_of_birth" type="date" class="form-control">
+                                    <input id="date_of_birth" type="date"
+                                        class="form-control @error('date_of_birth') is-invalid @enderror"
+                                        value="{{ old('date_of_birth') }}">
                                 </div>
+                                @error('date_of_birth')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="mb-4
                                         row">
