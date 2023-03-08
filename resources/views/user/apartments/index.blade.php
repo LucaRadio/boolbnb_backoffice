465c12283317926d9edbcb617a1b5c75089beea2
gab-che <gabriellachessa1@gmail.com>
2023-03-08 16:38:30 +0100

    fixes images in index card

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -8,8 +8,7 @@
                 <div class="col col-sm-6 col-md-4">
                     <div class="col-content">
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src={{ 'public/storage/cover_img/NoImageFound.jpg.png' }}
-                                alt="Card image cap">
+                            <img class="card-img-top" src={{ asset('storage/' . $item['img_cover']) }} alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $item->title }}</h5>
                                 <p class="card-text">{{ $item->description }}</p>
