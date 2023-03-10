2e376c88e94a3fbfc8a9fb0ab0c337f3186bac08
gab-che <gabriellachessa1@gmail.com>
2023-03-10 10:31:40 +0100

    adds sponsored apartments api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\Promotion;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
     public function index(Request $request)
     {
+        $allApartments = Apartment::all();
         $sponsored = $request->input('sponsored');
 
         if ($sponsored) {
-            $apartments = Apartment::with('promotions');
+            foreach ($allApartments as $item) {
+                $apartments = $item
+                    ->whereHas('promotions', function ($q) {
+                        $q->where('expired_at', '>', Carbon::now());
+                    })
+                    ->get();
+            };
         } else {
             //aggiungere raggio default 20km?
             $apartments = Apartment::with('services')->paginate(5);
