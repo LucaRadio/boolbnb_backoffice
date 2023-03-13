97648b051942ab2ab37c307dcbafb93eb12d364c
gab-che <gabriellachessa1@gmail.com>
2023-03-13 09:53:19 +0100

    adjustments

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -24,22 +24,23 @@ class ApartmentController extends Controller
                     ->whereHas('promotions', function ($q) {
                         $q->where('expired_at', '>', Carbon::now());
                     })
-                    ->get();
+                    ->get()
+                    ->sortByDesc('promotions.id');
             };
         } else if ($nearest) {
             //trasforma input in coordinate
-            $via = urlencode($nearest);
-            $rawData = file_get_contents("https://api.tomtom.com/search/2/geocode/" . $via . ".json?storeResult=false&countrySet=IT&view=Unified&limit=1&key=sGNJHBIkBGVklWlAnKDehryPD39qsJxn");
+            $city = urlencode($nearest);
+            $rawData = file_get_contents("https://api.tomtom.com/search/2/geocode/" . $city . ".json?storeResult=false&countrySet=IT&view=Unified&limit=1&key=sGNJHBIkBGVklWlAnKDehryPD39qsJxn");
             $rawData = json_decode($rawData);
             $lat = $rawData->results[0]->position->lat;
             $lon = $rawData->results[0]->position->lon;
 
-            //filtro raggio 20km
             $nearestApartments = [];
-            foreach ($allCoordinates as $coordinate) {
-                $radiusSearch =  file_get_contents("https://api.tomtom.com/search/2/geometryFilter.json?geometryList=%5B%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22" . $lat . "%2C%20" . $lon . "%22%2C%20%22radius%22%3A20000%7D%5D&poiList=%5B%7B%22position%22%3A%7B%22lat%22%3A" . $coordinate['latitude'] . "%2C%22lon%22%3A" . $coordinate['longitude'] . "%7D%7D%5D&key=1p9OyCRm8S7icw73fBmkTYDlXYJGPO9O");
+            //ciclo su tutte le coordinate a db
+            foreach ($allCoordinates as $coordinates) {
+                $radiusSearch =  file_get_contents("https://api.tomtom.com/search/2/geometryFilter.json?geometryList=%5B%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22" . $lat . "%2C%20" . $lon . "%22%2C%20%22radius%22%3A20000%7D%5D&poiList=%5B%7B%22position%22%3A%7B%22lat%22%3A" . $coordinates['latitude'] . "%2C%22lon%22%3A" . $coordinates['longitude'] . "%7D%7D%5D&key=1p9OyCRm8S7icw73fBmkTYDlXYJGPO9O");
                 $radiusSearch = json_decode($radiusSearch);
-
+                //se c'è un match pusho coordinate e relativo appart. in array
                 if (!empty($radiusSearch->results)) {
                     array_push($nearestApartments, Apartment::where([
                         ['latitude', $radiusSearch->results[0]->position->lat],
