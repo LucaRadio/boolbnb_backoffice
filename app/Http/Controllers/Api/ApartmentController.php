f56a3aed22bb0e56ab1c545b4b0f13d5f6010502
gab-che <gabriellachessa1@gmail.com>
2023-03-14 11:31:39 +0100

    fixes api request

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -29,6 +29,46 @@ class ApartmentController extends Controller
                     ->get()
                     ->sortByDesc('promotions.id');
             };
+        } else if ($simpleSearch) {
+            //trasforma input in coordinate
+            $city = urlencode($simpleSearch);
+            $rawData = file_get_contents("https://api.tomtom.com/search/2/geocode/" . $city . ".json?storeResult=false&countrySet=IT&view=Unified&limit=1&key=sGNJHBIkBGVklWlAnKDehryPD39qsJxn");
+            $rawData = json_decode($rawData);
+            $lat = $rawData->results[0]->position->lat;
+            $lon = $rawData->results[0]->position->lon;
+            //ordina tutte le coordinate a db a distanza crescente da input utente
+            $allCoordinates = Apartment::select('latitude', 'longitude')
+                ->selectRaw('(6371 * acos (
+                    cos ( radians(?) )
+                    * cos( radians( latitude ) )
+                    * cos( radians( longitude ) - radians(?) )
+                    + sin ( radians(?) )
+                    * sin( radians( latitude )))) AS distance', [
+                    $lat,
+                    $lon,
+                    $lat
+                ])
+                ->orderBy('distance', 'ASC')
+                ->get()
+                ->toArray();
+
+            $nearestApartments = [];
+            //ciclo su tutte le coordinate a db
+            foreach ($allCoordinates as $coordinates) {
+                $radiusSearch =  file_get_contents("https://api.tomtom.com/search/2/geometryFilter.json?geometryList=%5B%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22" . $lat . "%2C%20" . $lon . "%22%2C%20%22radius%22%3A20000%7D%5D&poiList=%5B%7B%22position%22%3A%7B%22lat%22%3A" . $coordinates['latitude'] . "%2C%22lon%22%3A" . $coordinates['longitude'] . "%7D%7D%5D&key=1p9OyCRm8S7icw73fBmkTYDlXYJGPO9O");
+                $radiusSearch = json_decode($radiusSearch);
+
+                //se c'è un match pusho coordinate e relativo appart. in array
+                if (!empty($radiusSearch->results)) {
+                    array_push($nearestApartments, Apartment::where([
+                        ['latitude', $radiusSearch->results[0]->position->lat],
+                        ['longitude', $radiusSearch->results[0]->position->lon]
+                    ])
+                        ->with('services')
+                        ->get());
+                }
+            }
+            $apartments = $nearestApartments;
         } else if ($advancedSearch) {
             $city = urlencode($advancedSearch['place']);
             $rawData = file_get_contents("https://api.tomtom.com/search/2/geocode/" . $city . ".json?storeResult=false&countrySet=IT&view=Unified&limit=1&key=sGNJHBIkBGVklWlAnKDehryPD39qsJxn");
@@ -72,46 +112,6 @@ class ApartmentController extends Controller
                 }
             }
             $apartments = $advancedApartments;
-        } else if ($simpleSearch) {
-            //trasforma input in coordinate
-            $city = urlencode($simpleSearch);
-            $rawData = file_get_contents("https://api.tomtom.com/search/2/geocode/" . $city . ".json?storeResult=false&countrySet=IT&view=Unified&limit=1&key=sGNJHBIkBGVklWlAnKDehryPD39qsJxn");
-            $rawData = json_decode($rawData);
-            $lat = $rawData->results[0]->position->lat;
-            $lon = $rawData->results[0]->position->lon;
-            //ordina tutte le coordinate a db a distanza crescente da input utente
-            $allCoordinates = Apartment::select('latitude', 'longitude')
-                ->selectRaw('(6371 * acos (
-                    cos ( radians(?) )
-                    * cos( radians( latitude ) )
-                    * cos( radians( longitude ) - radians(?) )
-                    + sin ( radians(?) )
-                    * sin( radians( latitude )))) AS distance', [
-                    $lat,
-                    $lon,
-                    $lat
-                ])
-                ->orderBy('distance', 'ASC')
-                ->get()
-                ->toArray();
-
-            $nearestApartments = [];
-            //ciclo su tutte le coordinate a db
-            foreach ($allCoordinates as $coordinates) {
-                $radiusSearch =  file_get_contents("https://api.tomtom.com/search/2/geometryFilter.json?geometryList=%5B%7B%22type%22%3A%22CIRCLE%22%2C%20%22position%22%3A%22" . $lat . "%2C%20" . $lon . "%22%2C%20%22radius%22%3A20000%7D%5D&poiList=%5B%7B%22position%22%3A%7B%22lat%22%3A" . $coordinates['latitude'] . "%2C%22lon%22%3A" . $coordinates['longitude'] . "%7D%7D%5D&key=1p9OyCRm8S7icw73fBmkTYDlXYJGPO9O");
-                $radiusSearch = json_decode($radiusSearch);
-
-                //se c'è un match pusho coordinate e relativo appart. in array
-                if (!empty($radiusSearch->results)) {
-                    array_push($nearestApartments, Apartment::where([
-                        ['latitude', $radiusSearch->results[0]->position->lat],
-                        ['longitude', $radiusSearch->results[0]->position->lon]
-                    ])
-                        ->with('services')
-                        ->get());
-                }
-            }
-            $apartments = $nearestApartments;
         } else {
             $apartments = Apartment::with('services')->paginate(5);
         }
