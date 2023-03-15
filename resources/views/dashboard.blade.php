78286b850a1fed834807f565782f1e2f7e2e37f1
gab-che <gabriellachessa1@gmail.com>
2023-03-15 22:19:43 +0100

    improves dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,9 @@
     $messaggi = 0;
 @endphp
 @section('content')
-    <div class="container">
+    <div class="container dashboard_container">
         <div class="row justify-content-center">
-            <div class="col-md-6 m-auto mb-5">
+            <div class="col-md-8 m-auto mb-5">
                 <h2 class="fs-4 text-secondary my-4">
                     {{ __('Dashboard') }}
                 </h2>
@@ -25,50 +25,70 @@
                             </div>
                         @endif
 
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="card p-3 mb-3">
-                                    <a href="{{ route('user.messages.index') }} " class="text-decoration-none text-black">
-                                        <span>Hai
-                                            @foreach (Auth::user()->apartments as $apartment)
-                                                @php
-                                                    $messaggi = $messaggi + $apartment->messages->count();
-                                                @endphp
-                                            @endforeach
-                                            @if ($messaggi === 1)
-                                                {{ $messaggi }} messaggio
-                                            @else
-                                                {{ $messaggi }} messaggi
-                                            @endif
-                                        </span>
-                                        <img src="{{ url('/1.png') }}" alt="" class="img-fluid">
-                                    </a>
+                        <div class="row gap-3 gap-lg-0">
+                            <div class="col-lg-4">
+                                <div class="row flex-column gap-3">
+                                    <div class="col">
+                                        <a href="{{ route('user.messages.index') }} "
+                                            class="text-decoration-none text-black">
+                                            <div class="card p-3">
+                                                <div class="card_title">
+                                                    <h5>Hai
+                                                        @foreach (Auth::user()->apartments as $apartment)
+                                                            @php
+                                                                $messaggi = $messaggi + $apartment->messages->count();
+                                                            @endphp
+                                                        @endforeach
+                                                        @if ($messaggi === 1)
+                                                            {{ $messaggi }} messaggio
+                                                        @else
+                                                            {{ $messaggi }} messaggi
+                                                        @endif
+                                                    </h5>
+                                                </div>
+                                                <div class="card_img d-flex justify-content-center">
+                                                    <img src="{{ url('/1.png') }}" alt="" class="w-50 img-fluid">
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="#" class="text-decoration-none text-black">
+                                            <div class="card  p-3">
+                                                <div class="card_title">
+                                                    <h5 class="">Statistiche</h5>
+                                                </div>
+                                                <div class="card_img d-flex justify-content-center">
+                                                    <img src="{{ url('/2.png') }}" alt="" class="w-50 img-fluid">
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="card p-3 mb-3">
-                                    <a href="{{ route('user.apartments.index') }}" class="text-decoration-none text-black">
-                                        <span class="">I tuoi appartamenti</span>
-                                        <img src="{{ url('/3.png') }}" alt="" class="img-fluid">
-                                    </a>
-                                </div>
+                            <div class="col-lg-4">
+                                <a href="{{ route('user.apartments.index') }}" class="text-decoration-none text-black">
+                                    <div class="card p-3 h-100  d-flex justify-content-between">
+                                        <div class="card_title">
+                                            <h5 class="">I tuoi appartamenti</h5>
+                                        </div>
+                                        <div class="card_img">
+                                            <img src="{{ url('/3.png') }}" alt="" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="card  p-3 mb-3">
-                                    <a href="#" class="text-decoration-none text-black">
-                                        <span class="">Statistiche</span>
-                                        <img src="{{ url('/2.png') }}" alt="" class="img-fluid">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="card p-3 mb-3">
-                                    <a href="{{ route('user.apartments.create') }}"
-                                        class="text-decoration-none text-black">
-                                        <span class="">Crea un appartamento</span>
-                                        <img src="{{ url('/4.png') }}" alt="" class="img-fluid">
-                                    </a>
-                                </div>
+                            <div class="col-lg-4">
+                                <a href="{{ route('user.apartments.create') }}" class="text-decoration-none text-black">
+                                    <div class="card p-3 h-100 d-flex justify-content-between">
+                                        <div class="card_title">
+                                            <h5 class="">Crea un appartamento</h5>
+                                        </div>
+                                        <div class="card_img">
+                                            <img src="{{ url('/4.png') }}" alt="" class="img-fluid">
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
