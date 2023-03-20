e64f9f3ceebd691dc6dccfdce9b98fb5f0f21cc4
MarcoAgosta <agostamarco@libero.it>
2023-03-20 16:11:21 +0100

    Eliminazione messaggio

--- resources/views/user/messages/show.blade.php
+++ resources/views/user/messages/show.blade.php
@@ -11,6 +11,8 @@
                     da {{$message->sender_name}}
                 </span>
 
+                <span class="darkgray d-none d-sm-block">{{$message->apartment["title"]}}</span>
+
                 <span>
                     {{$message->email}}
                 </span>
@@ -22,6 +24,17 @@
                 <p class="m-2">
                     {{$message->message}}
                 </p>
+
+                <div class="d-flex justify-content-end align-items-center m-3">
+
+                    <form action="{{ route('user.messages.delete', $message) }}" method="POST"
+                        class="delete_apartment">
+                        @csrf
+                        @method('delete')
+                        <button class="btn btn-danger">Elimina messaggio</button>
+                    </form>
+
+                </div>
         </li>
         
         </ul>
@@ -35,5 +48,7 @@
 .titolo{
     background-color: rgb(251, 226, 146) !important;
 }
-
+.darkgray{
+    color: darkgray;
+}
 </style>
